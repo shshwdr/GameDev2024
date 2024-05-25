6c18a54e14da2e7658b11c6f4b64531e60d96ea1
flavedo <13405179+flavedo@user.noreply.gitee.com>
2024-05-25 22:12:13 +0800

    drag ingredient

---
+++ Assets/DragController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class DragController : MonoBehaviour
+{
+    public Ingredient draggingIngredient;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         if (Input.GetMouseButtonDown(0))
+         {
+             if( draggingIngredient == null)
+             {
+                 
+                 // Get the mouse position in world coordinates
+
+                 // Cast a ray from the mouse position
+                 RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+                 if (hit)
+                 {
+                     var ingredient = hit.transform.GetComponent<Ingredient>();
+                     if (ingredient != null)
+                     {
+                         
+                         var draggingingredient = Instantiate(Resources.Load<GameObject>("Ingredient/ingredient"),ingredient.transform.position,quaternion.identity);
+                         draggingingredient.GetComponent<Ingredient>().Init(ingredient.GetComponent<Ingredient>().Info);
+                         
+                         draggingIngredient = draggingingredient.GetComponent<Ingredient>();
+                     }
+                 }
+             }
+         }
+
+         if (Input.GetMouseButton(0))
+         {
+             
+             if (draggingIngredient != null)
+             {
+                 draggingIngredient.transform.position = mousePosition;
+             }
+         }
+    }
+}
--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
+    IngredientInfo info;
+    public IngredientInfo Info => info;
     public SpriteRenderer spriteRenderer;
     public void Init(IngredientInfo info)
     {
+        this.info = info;
         spriteRenderer.sprite = Resources.Load<Sprite>("Ingredient/" + info.name);
     }
 }
